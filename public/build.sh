b44d7b55c033210a66b6469dd3f9846ec119ddc4
The Maximalist <hello@themaximalist.com>
2024-02-22 00:24:05 -0800

    site

--- public/build.sh
+++ public/build.sh
@@ -2,7 +2,7 @@
 #
 NAME="vectordb.js"
 DOMAIN="vectordbjs.themaximalist.com"
-TITLE="VectorDB.js — Simple embeddings and vector search for JavaScript"
+TITLE="VectorDB.js — Simple in-memory vector database for Node.js"
 DESCRIPTION="VectorDB.js is the simplest way to do text similarity in Node.js"
 
 readme-to-index "$NAME" "$DOMAIN" "$TITLE" "$DESCRIPTION"
